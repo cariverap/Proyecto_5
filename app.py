11cc041c950b29553bc84827d4a9376aca7bc1cb
Cats <cariverap98@gmail.com>
2024-04-10 10:57:18 -0400

    app script

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import plotly.express as px
 st.header('Anuncios de venta de Automóviles')
 
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
-# hist_button = st.button('Construir histograma')  # crear un botón
-build_histogram = st.checkbox('Construir un histograma')
+
+build_histogram = st.checkbox('Construir un histograma')  # crear una casilla
 # se creara un histograma que distribuya la cantidad de kilometraje que poseen los autos
-# if hist_button:  # al hacer clic en el botón
+
 if build_histogram:
     # escribir un mensaje
     st.write(
@@ -22,7 +22,8 @@ if build_histogram:
     st.plotly_chart(fig, use_container_width=True)
 
 # Se construye un grafico de dispersión de la variacion de los precios de automoviles en función del kilometraje
-# scat_button = st.button('Construir grafico de dispersión')  # crear un botón
+
+# se creauna casilla de verificación
 build_scatter = st.checkbox('Construir un grafico de dispersión')
 # if scat_button:  # al hacer clic en el botón
 if build_scatter:
@@ -31,7 +32,7 @@ if build_scatter:
         'Creación de un grafico de dispesión para el conjunto de datos de anuncios de venta de coches, precio vs kilometraje')
 
     # crear un histograma
-    fig = px.scatter(car_data, x="odometer", y="price")
+    fig = px.scatter(car_data, x="odometer", y="price", color='petal_length')
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
