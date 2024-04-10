d9ffd2f825bf6de2e0aafba025f5776825c673e7
Cats <cariverap98@gmail.com>
2024-04-10 10:31:36 -0400

    script

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ st.header('Anuncios de venta de Automóviles')
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 hist_button = st.button('Construir histograma')  # crear un botón
 
+# se creara un histograma que distribuya la cantidad de kilometraje que poseen los autos
 if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
     st.write(
@@ -15,6 +16,20 @@ if hist_button:  # al hacer clic en el botón
 
     # crear un histograma
     fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+# Se construye un grafico de dispersión de la variacion de los precios de automoviles en función del kilometraje
+scat_button = st.button('Construir grafico de dispersión')  # crear un botón
+
+if scat_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un grafico de dispesión para el conjunto de datos de anuncios de venta de coches, precio vs kilometraje')
+
+    # crear un histograma
+    fig = px.scatter(car_data, x="odometer", y="price")
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
